28d52fb6ec56f2f1aaeb5b0a4ccdd6cf5cc7d230
MikO <miko007@me.com>
2015-06-12 23:33:02 +0200

    added footer

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 <?php
 	wp_footer();
 ?>
+<footer>
+	Copyright &copy; 2015, <a href="<?php echo get_bloginfo('url') ?>"><?php echo get_bloginfo('name') ?></a>
+</footer>
 <nav class="socialIcons">
 	<ul>
 		<li><a href="http://facebook.com" class="fa fa-facebook"></a></li>
