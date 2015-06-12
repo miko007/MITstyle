d94dcc72d97ba812e72b33eab3920c24ecdcfe72
MikO <miko007@me.com>
2015-06-12 19:06:58 +0200

    added social icons design

--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
 <?php
 	wp_footer();
 ?>
+<nav class="socialIcons">
+	<ul>
+		<li><a href="http://facebook.com" class="fa fa-facebook"></a></li>
+		<li><a href="http://facebook.com" class="fa fa-twitter"></a></li>
+		<li><a href="http://facebook.com" class="fa fa-youtube"></a></li>
+		<li><a href="http://facebook.com" class="fa fa-rss"></a></li>
+	</ul>
+</nav>
 </body>
 </html>
